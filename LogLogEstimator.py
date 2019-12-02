e6988028a96501114f32dc582ad1798810e3c179
Sam Braga <sambraga87@gmail.com>
2019-12-02 17:42:47 -0500

    Update LogLogEstimator.py
    LogLog algorithm

--- LogLogEstimator.py
+++ LogLogEstimator.py
@@ -1,1 +1,26 @@
 # Sam Braga
+def trailing_zeroes(num):
+  """Counts the number of trailing 0 bits in num."""
+  if num == 0:
+    return 32 # Assumes 32 bit integer inputs!
+  p = 0
+  while (num >> p) & 1 == 0:
+    p += 1
+  return p
+
+def loglog(values, k):
+  """Estimates the number of unique elements in the input set values.
+
+  Arguments:
+    values: An iterator of hashable elements to estimate the cardinality of.
+    k: The number of bits of hash to use as a bucket number; there will be 2**k buckets.
+  """
+  num_buckets = 2 ** k
+  max_zeroes = [0] * num_buckets
+  for value in values:
+    h = hash(value)
+    bucket = h & (num_buckets - 1) # Mask out the k least significant bits as bucket ID
+    bucket_hash = h >> k
+    max_zeroes[bucket] = max(max_zeroes[bucket], trailing_zeroes(bucket_hash))
+
+  return 2 ** (float(sum(max_zeroes)) / num_buckets) * num_buckets * 0.79402
