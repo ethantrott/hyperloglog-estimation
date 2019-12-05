e9afaf098446009ecc61db0bcd90fcd267d1bbb2
ethantrott <thatsmyface12@gmail.com>
2019-12-05 13:49:18 -0500

    tidied up some comments and output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# LogLog, SuperLogLog, and HyperLogLog Demo
+# The Evolution of the HyperLogLog Estimator
 # Dawsin Blanchard, Sam Braga, Brian Couture, and Ethan Trott
 # COS226 University of Maine
 
@@ -15,9 +15,11 @@ ex_buckets = 8
 # number of runs to average with different hashes
 loops_to_run = 8
 
-print ("LogLog, SuperLogLog, and HyperLogLog Demo")
+print ("The Evolution of the HyperLogLog Estimator")
 print ("by Dawsin Blanchard, Sam Braga, Brian Couture, and Ethan Trott\n")
 
+# check if the Plate ID hashes have been generated before
+# if so, load them. if not, create them
 plate_hashes = []
 if os.path.exists("hashes.csv"):
     print("Loading plate hashes from file..")
@@ -28,42 +30,41 @@ else:
         print("Error: Please download the dataset from https://data.cityofnewyork.us/api/views/faiq-9dfq/rows.csv?accessType=DOWNLOAD and place it in this directory as "+filename)
         exit()
         
-    print("Creating plate hashes..")
+    print("Creating plate hashes.. (this may take a moment)")
     plate_hashes = csv_parser.create_hashes(filename)
 print(str(len(plate_hashes))+" plate hashes loaded.\n")
-
 
 # directly tally the number of unique plates
 print("Calculating exact amount of unique plates..")
 exact_unique = ActualCount.get_exact_unique_using_set(filename)
 print("There are exactly "+str(exact_unique)+" unique plates..\n")
 
-#run tests 8 times
+#run tests and store results
 fm = []
 ll = []
 hll = []
-for i in range(0, 8):
-  #print loading message
-  sys.stdout.write("Running test " + str(i + 1) + " of 8...")
-  sys.stdout.flush()
-  
-  #Generate test hashes
-  hashes = []
-  for hash in plate_hashes:
-    #encode the hash in binary
-    hash = '{:256b}'.format(int(hash, 16)).replace(' ','0')
-    
-    off = i*32
-    temp = hash[len(hash)-(32+off)+1:len(hash)-off]
-    hashes.append(temp)
-  
-  #add to results array
-  fm.append(FlajoletMartinEstimator.FlajoletMartin(hashes))
-  ll.append(LogLogEstimator.LogLog(hashes, ex_buckets))
-  hll.append(HyperLogLogEstimator.HyperLogLog(hashes, ex_buckets))
+for i in range(0, loops_to_run):
+    #print loading message
+    sys.stdout.write("Running test " + str(i + 1) + " of " + str(loops_to_run) + ".. (this may take a moment)")
+    sys.stdout.flush()
 
-  #reset line
-  sys.stdout.write('\r')
+    #Generate test hashes
+    hashes = []
+    for hash in plate_hashes:
+        #encode the hash in binary
+        hash = '{:256b}'.format(int(hash, 16)).replace(' ','0')
+
+        off = i*32
+        temp = hash[len(hash)-(32+off)+1:len(hash)-off]
+        hashes.append(temp)
+
+    #approximate values and add to results array
+    fm.append(FlajoletMartinEstimator.FlajoletMartin(hashes))
+    ll.append(LogLogEstimator.LogLog(hashes, ex_buckets))
+    hll.append(HyperLogLogEstimator.HyperLogLog(hashes, ex_buckets))
+
+    #reset line
+    sys.stdout.write('\r')
 
 #test formatter
 def printTest(label, expected, actual):
